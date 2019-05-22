4227204a51feca56d32ed90ce1bf77633b086564
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2019-05-22 21:18:45 +0300

    Add redirect to login on expired JWT

--- team-travel/src/components/Authorization/Authorization.js
+++ team-travel/src/components/Authorization/Authorization.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { parseJwt } from "../services/localStorage";
 import { Redirect } from "react-router";
+import moment from "moment";
 
 export const Authorization = (WrappedComponent, allowed) => class AuthController extends Component {
     render() {
         let user;
         try {
             user = parseJwt();
+            if(moment().isAfter(moment.unix(user.exp))) {
+                return <Redirect to='/login'/>;
+            }
         } catch (error) {
             return <Redirect to='/login'/>;
         }
